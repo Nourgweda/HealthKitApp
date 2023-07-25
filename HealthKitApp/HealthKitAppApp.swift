df2eee55ffbfcb999a1bbc22e2c6ffa4c358ae82
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2023-07-25 21:29:15 +0300

    Initial Commit

---
+++ HealthKitApp/HealthKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitAppApp.swift
+//  HealthKitApp
+//
+//  Created by Nour Gweda on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
